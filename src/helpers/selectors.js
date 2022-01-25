7a5b2a11a2682ea2b3284082cbc56dbdc75638f4
Michael <kmiecik013@gmail.com>
2022-01-25 16:03:01 -0700

    updated selector fucntion getInterviewersDay to find the interviewers instead of filtering through them

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,11 @@
 function getAppointmentsForDay(state, day) {
-
+  
 
   const filteredAppointments = [];
 
-  const filteredDay = state.days.filter((weekday) => weekday.name === day)[0]; // {} | undefined
+  const filteredDay = state.days.find((weekday) => weekday.name === day); // {} | undefined
 
+    console.log("check fileteredday" , filteredDay);
 
   if (!filteredDay) {
     return [];
@@ -32,6 +33,33 @@ function getInterview(state, interview) {
 };
 
 
+
+function getInterviewersForDay(state, day) {
+  
+
+  const filteredInterviewers = [];
+
+  const filteredDay = state.days.find((weekday) => weekday.name === day); // {} | undefined
+
+    console.log("check fileteredday" , filteredDay);
+
+  if (!filteredDay) {
+    return [];
+  }
+
+  filteredDay.interviewers.forEach((interviewer) => {
+    const matchedInterviewer = state.interviewers[interviewer]; // { id: 4, time: "3pm", interview: null } | undefined
+    if (matchedInterviewer) {
+      filteredInterviewers.push(matchedInterviewer);
+    }
+  });
+
+  return filteredInterviewers;
+}
+
+
+/*
+
 function getInterviewersForDay(state, day) {
 
 const appointments = getAppointmentsForDay(state, day) 
@@ -44,8 +72,6 @@ const interviewers = appointmentWithInterviews.map((appointment) => {
   
   return state.interviewers[interviewerID]
 
-
-  
 })
 console.log(interviewers);
 //console.log("check appointment", appointments);
@@ -53,8 +79,37 @@ console.log(interviewers);
 
 return interviewers;
 
-
 }
+*/
+
+/*
+function getInterviewersForDay(state, day) {
+
+
+  //const filteredAppointments = [];
+  const interviewers = [];
+  
+  
+  //const filteredDay = state.days.find((weekday) => weekday.name === day)[0]; // {} | undefined
+  const foundDay = state.days.find((weekday) => weekday.name === day); // {} | undefined
+  console.log("state", state);
+
+
+  if (!foundDay) {
+    return [];
+  }
+
+  foundDay.appointments.forEach((appointment) => {
+    const matchedAppointment = state.appointments[appointment]; // { id: 4, time: "3pm", interview: null } | undefined
+    if (matchedAppointment) {
+      interviewers.push(matchedAppointment);
+    }
+  });
+
+  return interviewers;
+}
+*/
+
 
 export { getAppointmentsForDay, getInterview, getInterviewersForDay };
 
